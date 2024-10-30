abab26e28853ab490bcd2790168e9d050aea7f27
Benjamin <b.pondville@hotmail.com>
2024-10-30 10:10:42 +0400

    creation of button

---
+++ components/btn/ButtonRed.vue
@@ -0,0 +1,41 @@
+<script setup>
+const props = defineProps({
+  btnText: {
+    type: String,
+    default: "Click me",
+    required: true,
+  },
+  btnColor: {
+    type: String,
+    default: "black",
+    required: true,
+  },
+  btnPicto: {
+    type: String,
+    default: "🔴",
+    required: true,
+  },
+  btnLink: {
+    type: String,
+    default: "/",
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <a :href="btnLink" class="btn" :style="{ color: btnColor }">
+    <span>{{ btnPicto }}</span>
+    <span>{{ btnText }}</span>
+  </a>
+</template>
+
+<style lang="scss" scoped>
+.btn {
+  background-color: red;
+
+  &:hover {
+    background-color: blue;
+  }
+}
+</style>
