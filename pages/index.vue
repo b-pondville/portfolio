d05fc98458ee839a9d60e142e0891d5bd6424c55
Benjamin <b.pondville@hotmail.com>
2024-10-30 10:11:07 +0400

    installation of sass

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+
+<template>
+  <main>
+    <h1>Home page</h1>
+    <p>Welcome to the home page</p>
+    <btnButtonRed
+      :btnText="'Hi'"
+      :btnColor="'red'"
+      :btnPicto="'X '"
+      :btnLink="'/'"
+    />
+  </main>
+</template>
+
+<style scoped></style>
