7ddc17d03023d987ad3f513d474afbd0482d8980
Benjamin <b.pondville@hotmail.com>
2024-11-05 10:12:05 +0400

    ssr true

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  ssr: true,
   css: ["@/assets/styles/main.scss"],
   modules: ["@nuxtjs/google-fonts", "@nuxt/image", "@vueuse/nuxt"],
   googleFonts: {
